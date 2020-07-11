6aaa6cc8dfc7d688e54ca62c66c00a24d221c823
Dominic Mazza <mazzadom@msu.edu>
2020-07-11 19:28:10 -0400

    Added entity class

---
+++ entity.py
@@ -0,0 +1,14 @@
+from typing import Tuple
+
+# This is a generic object that represents players, enemies items, etc.
+class Entity:
+    def __init__(self, x: int, y: int, char: str, color: Tuple[int, int, int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    # just moves the entity
+    def move(self, dx: int, dy:int):
+        self.x += dx
+        self.y += dy
